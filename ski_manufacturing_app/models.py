a5b3ff9e339e3c93b858f9b79c450d3649a94fbd
John Doe <johndoe@email.com>
2025-04-20 12:15:30 -0600

    deleted raw material, returns, and reports from models

--- ski_manufacturing_app/models.py
+++ ski_manufacturing_app/models.py
@@ -24,13 +24,6 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-class RawMaterial(models.Model):
-    type = models.CharField(max_length=200)
-    quantity_available = models.IntegerField()
-
-    def __str__(self):
-        return self.type
-
 class Payment(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     payment_method = models.CharField(max_length=50)
