7f1d93f8bd528ea517d88f64b4d8e9300cd70edc
Junruiyi Xie <u1395976@utah.edu>
2025-04-20 19:13:08 -0600

    add the correct homepage URL pattern to URL configuration in template_urls.py

--- ski_manufacturing_app/template_urls.py
+++ ski_manufacturing_app/template_urls.py
@@ -5,6 +5,7 @@ from .views import *
 #these are all template urls go to the settings.py in project folder to see more info.
 #all urls in this page start with products/ before the actual url listed. 
 urlpatterns = [
+    path('homepage/', HomepageView.as_view(), name='home-page'),
     path('products/', ProductListPageView.as_view(), name='product-page'),
     path('products/add/', ProductAddView.as_view(), name='add-product'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
@@ -13,7 +14,6 @@ urlpatterns = [
     path('orders/<int:pk>/', OrderEditView.as_view(), name='order-edit'),
     path('orders/delete/<int:pk>/', OrderDeleteView.as_view(), name='delete-order'),
     path('buy_product/<int:pk>/', BuyProductView.as_view(), name='buy_product'),
-    path('homepage', HomepageView.as_view(), name='home-page'),
     path('users/', UserListPageView.as_view(), name='user-page'),
     
 ]
